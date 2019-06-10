a3a7e4f5112179b8da230ccd63252f535adf8588
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-10 16:09:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,16 @@ def countdown(int)
   while time > 0
     puts "#{time} SECOND(S)!"
     time -= 1
-    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int, pause)
+  time = int
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+    sleep(pause)
   end
   "HAPPY NEW YEAR!"
 end
